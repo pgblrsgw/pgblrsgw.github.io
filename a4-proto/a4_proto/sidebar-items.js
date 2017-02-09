d6fdd2a625a4f8d54d048dd8330119d2df9a34ae
Geordon Worley <vadixidav@gmail.com>
2017-02-09 11:19:23 -0500

    add public types for a4

--- a4-proto/a4_proto/sidebar-items.js
+++ a4-proto/a4_proto/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({});
+initSidebarItems({"enum":[["Shape","Describes the shape of an obstacle."]],"struct":[["Obstacle","Describes an obstacle to be added to the pathfinding algorithm."]]});
